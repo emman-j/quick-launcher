d25d6eae2b9be0532f9f2151c9b9916cbe604aa2
emman-j <176351546+emman-j@users.noreply.github.com>
2025-06-05 13:39:41 +0800

    added global paths

---
+++ quick-launcher/Library/Common/Globals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quick_launcher.Library.Common
+{
+    public class Globals
+    {
+        public class Paths
+        {
+            //Root path for the application
+            public static string AppDataPath { get => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "IMMS"); }
+            public static string DbPath { get => Path.Combine(AppDataPath, "bin", "data"); }
+        }
+    }
+}
