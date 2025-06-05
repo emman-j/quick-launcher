31983919cef8c24a2140b14b65c7326e78d530de
emman-j <176351546+emman-j@users.noreply.github.com>
2025-06-05 13:40:11 +0800

    added file object

---
+++ quick-launcher/Library/Objects/FileObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quick_launcher.Library.Objects
+{
+    public class FileObject
+    {
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public string Directory { get; set; }
+        public DateTime LastModified { get; set; }
+        public long Size { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FileName} ({Size} bytes) - Last Modified: {LastModified}";
+        }
+    }
+}
