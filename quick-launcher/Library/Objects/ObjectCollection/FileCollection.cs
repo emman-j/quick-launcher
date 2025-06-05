635a1a563f28ed5fc1455535842c5232ab88e6e9
emman-j <176351546+emman-j@users.noreply.github.com>
2025-06-05 13:40:20 +0800

    added file collection

---
+++ quick-launcher/Library/Objects/ObjectCollection/FileCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quick_launcher.Library.Objects.ObjectCollection
+{
+    public class FileCollection : List<FileObject>
+    {
+
+    }
+}
