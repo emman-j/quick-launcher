60fa9ba9181d41d938348578a207ea21f38a0afe
emman-j <176351546+emman-j@users.noreply.github.com>
2025-06-05 13:41:00 +0800

    added client and implementation of classes

---
+++ quick-launcher/Library/Client.cs
@@ -0,0 +1,24 @@
+﻿using quick_launcher.Library.Common;
+using quick_launcher.Library.Data;
+using quick_launcher.Library.DataAccess;
+using quick_launcher.Library.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quick_launcher.Library
+{
+    public class Client
+    {
+        public SQLiteDB IndexDB { get; set; }
+        public Indexer Indexer { get; set; }
+
+        public Client()
+        {
+            IndexDB = new SQLiteDB(Globals.Paths.DbPath, "indexDB");
+            Indexer = new Indexer(IndexDB);
+        }
+    }
+}
